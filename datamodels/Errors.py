dcb06f385bdbef0ca535c0487ac3e651501e9131
Dymidless <dymidless@gmail.com>
2026-03-23 19:53:44 +0100

    docs: add module and class docstrings to datamodels

--- datamodels/Errors.py
+++ datamodels/Errors.py
@@ -1,7 +1,20 @@
+"""
+Error tracking data model.
+
+This module provides the data model for storing and retrieving
+error logs from the database.
+"""
+
 from discord.ext import commands
 import discord
 from utils.mongo import Document
 
 
 class Errors(Document):
+    """
+    Data model for error logs.
+    
+    Stores error information including error messages, timestamps,
+    and unique error IDs for tracking and debugging purposes.
+    """
     pass
